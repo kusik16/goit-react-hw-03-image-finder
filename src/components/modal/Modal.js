23a0d0adfc3ee9dcf56ade9e48b841681bd3a716
kusik16 <slobodyanuck@gmail.com>
2022-09-23 15:41:01 +0300

    rework modal for portal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,9 +1,13 @@
+import { createPortal } from 'react-dom';
+
 import PropTypes from 'prop-types';
 
 import modal from './Modal.module.css';
 
+const modalRoot = document.querySelector('#modal-root');
+
 const Modal = ({ largeImageURL, closeModal, onEscapePress }) => {
-  return (
+  return createPortal(
     <div
       className={modal.overlay}
       onKeyPress={e => onEscapePress(e)}
@@ -12,7 +16,8 @@ const Modal = ({ largeImageURL, closeModal, onEscapePress }) => {
       <div className={modal.modal}>
         <img src={largeImageURL} alt="someimage" />
       </div>
-    </div>
+    </div>,
+    modalRoot
   );
 };
 
